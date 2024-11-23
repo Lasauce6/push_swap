9cafa0f0b9b8bcc3d1ab3bd73dad5f3da0e44afe
Raphael Baticle <rbaticle@made-f0Ar8s8.clusters.42paris.fr>
2024-11-23 13:48:28 +0100

    add operation push

--- operations.c
+++ operations.c
@@ -6,10 +6,11 @@
 /*   By: rbaticle <rbaticle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 10:23:08 by rbaticle          #+#    #+#             */
-/*   Updated: 2024/11/23 12:35:43 by rbaticle         ###   ########.fr       */
+/*   Updated: 2024/11/23 13:12:08 by rbaticle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
 #include "push_swap.h"
 
 int	swap(t_list	**stack)
@@ -27,8 +28,21 @@ int	swap(t_list	**stack)
 
 int	push(t_list	**stack_to, t_list **stack_from)
 {
+	t_list	*tmp;
+
 	if (!*stack_from)
 		return (1);
-
+	if (!*stack_to)
+	{
+		*stack_to = *stack_from;
+		*stack_from = (*stack_to)->next;
+		(*stack_to)->next = NULL;
+	}
+	else
+	{
+		tmp = (*stack_from)->next;
+		ft_lstadd_front(stack_to, *stack_from);
+		*stack_from = tmp;
+	}
 	return (0);
 }
