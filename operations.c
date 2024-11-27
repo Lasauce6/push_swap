e5863566a48e384cce2f0f0663afaa0d80bd0080
Raphael Baticle <rbaticle@made-f0Ar7s8.clusters.42paris.fr>
2024-11-27 14:35:57 +0100

    all operations

--- operations.c
+++ operations.c
@@ -6,14 +6,14 @@
 /*   By: rbaticle <rbaticle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 10:23:08 by rbaticle          #+#    #+#             */
-/*   Updated: 2024/11/23 13:12:08 by rbaticle         ###   ########.fr       */
+/*   Updated: 2024/11/27 14:35:44 by rbaticle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include "push_swap.h"
 
-int	swap(t_list	**stack)
+int	swap(t_list **stack)
 {
 	t_list	*tmp;
 
@@ -26,7 +26,7 @@ int	swap(t_list	**stack)
 	return (0);
 }
 
-int	push(t_list	**stack_to, t_list **stack_from)
+int	push(t_list **stack_to, t_list **stack_from)
 {
 	t_list	*tmp;
 
@@ -44,5 +44,35 @@ int	push(t_list	**stack_to, t_list **stack_from)
 		ft_lstadd_front(stack_to, *stack_from);
 		*stack_from = tmp;
 	}
+	return (0);
+}
+
+int	rotate(t_list **stack)
+{
+	t_list	*tmp;
+
+	if (!*stack)
+		return (1);
+	ft_lstlast(*stack)->next = *stack;
+	tmp = (*stack)->next;
+	(*stack)->next = NULL;
+	*stack = tmp;
+	return (0);
+}
+
+int	reverse_rotate(t_list **stack)
+{
+	t_list	*prev_last;
+	t_list	*tmp;
+
+	if (!*stack)
+		return (1);
+	prev_last = *stack;
+	while (prev_last->next->next)
+		prev_last = prev_last->next;
+	tmp = ft_lstlast(*stack);
+	prev_last->next = NULL;
+	tmp->next = *stack;
+	*stack = tmp;
 	return (0);
 }
